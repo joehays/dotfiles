1430e86bb26a5fc4afacb3d91ef9297860b34636
Joe Hays <hays.joe@gmail.com>
2025-02-17 17:31:58 -0500

    added support for both lua and luarocks

--- scripts/programs/lua.sh
+++ scripts/programs/lua.sh
@@ -27,19 +27,40 @@ echo '------------------------------'
 echo 'Cloning && building LUA'
 echo '------------------------------'
 
-if [ ! -d "${HOME}/apps/lua" ]; then
-  mkdir -p ${HOME}/apps
-  git clone git@github.com:lua/lua.git 
-  cd lua
-  make all test
+cd ${HOME}/apps
+curl -L -R -O https://www.lua.org/ftp/lua-5.4.7.tar.gz
+tar -zxf lua-5.4.7.tar.gz
+rm lua-5.4.7.tar.gz
+cd lua-5.4.7
+#make linux test
+install_top=${PWD}
+make CC="clang" CFLAGS="-O2 -fPIC -DLUA_USE_POSIX -DLUA_USE_DLOPEN" MYCFLAGS="-fno-stack-protector"
+make install INSTALL_TOP=${install_top}
+
+
+#if [ ! -d "${HOME}/apps/lua" ]; then
+#  mkdir -p ${HOME}/apps
+#  git clone git@github.com:lua/lua.git 
+#  cd lua
+#  install_top=${HOME}/apps/lua
+#
+#  #make all test
+#  if command -v termux-info >/dev/null 2>&1; then
+#    make CC="clang" CFLAGS="-O2 -fPIC -DLUA_USE_POSIX -DLUA_USE_DLOPEN" MYCFLAGS="-fno-stack-protector"
+#  else
+#    make all test
+#  fi
+#fi
+#
+#make install INSTALL_TOP=${install_top}
   
-  echo "${COND_INSERT} \"export PATH=\"\${PATH}:${HOME}/apps/lua\"\" ${HOME}/.zshrc"
-  ${COND_INSERT} "export PATH=\"\${PATH}:${HOME}/apps/lua\"" ${HOME}/.zshrc
+echo "${COND_INSERT} 'export PATH=\"${install_top}/bin:\${PATH}\"' \${HOME}/.zshrc"
+${COND_INSERT} "export PATH=\"${install_top}/bin:\${PATH}\"" ${HOME}/.zshrc
 
-  echo "${COND_INSERT} \"export PATH=\"\${PATH}:${HOME}/apps/lua\"\" ${HOME}/.bashrc"
-  ${COND_INSERT} "export PATH=\"\${PATH}:${HOME}/apps/lua\"" ${HOME}/.bashrc
-end
+echo "${COND_INSERT} 'export PATH=\"${install_top}/bin:\${PATH}\"' \${HOME}/.bashrc"
+${COND_INSERT} "export PATH=\"${install_top}/bin:\${PATH}\"" ${HOME}/.bashrc
 
-# TODO: delete downloaded files
 
 cd ${CWD}
+
+
---
+++ scripts/programs/luarocks.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+echo
+echo '============================================================'
+echo 'PROGRAMS/luarocks.SH'
+echo '============================================================'
+
+# get my own absolute path
+SCRIPT_ABS_DIR=$(dirname $(readlink -f -- "$0"; ));
+echo "SCRIPT_ABS_DIR = ${SCRIPT_ABS_DIR} "
+
+COND_INSERT="${SCRIPT_ABS_DIR}/../cond-insert-string-into-file"
+echo "COND_INSERT = ${COND_INSERT} "
+
+#${SCRIPT_ABS_DIR}/lua.sh
+
+CWD=$(pwd)
+cd ${HOME}/apps
+
+
+echo
+echo '------------------------------'
+echo 'Cloning && building luarocks'
+echo '------------------------------'
+
+wget https://luarocks.org/releases/luarocks-3.11.1.tar.gz
+tar zxpf luarocks-3.11.1.tar.gz
+rm luarocks-3.11.1.tar.gz
+cd luarocks-3.11.1
+./configure --prefix=${PWD} --with-lua=${HOME}/apps/lua-5.4.7
+make CC="clang" CFLAGS="-O2 -fPIC -DLUA_USE_POSIX -DLUA_USE_DLOPEN" MYCFLAGS="-fno-stack-protector"
+make install
+
+#if [ ! -d "${HOME}/apps/luarocks" ]; then
+#  mkdir -p ${HOME}/apps
+#  git clone git@github.com:luarocks/luarocks.git
+#  cd luarocks
+#  #if command -v termux-info >/dev/null 2>&1; then
+#  echo "${HOME}/apps/luarocks/configure  --with-lua-include=${HOME}/apps/lua"
+#  ${HOME}/apps/luarocks/configure  --with-lua-include=${HOME}/apps/lua
+#  #else
+#  #  ./configure --with-lua-include=/usr/local/include
+#  #fi
+#  make . 
+#  #make install .
+#fi
+
+echo "${COND_INSERT} 'export PATH=\"${PWD}/bin:\${PATH}\"' \${HOME}/.zshrc"
+${COND_INSERT} "export PATH=\"${PWD}/bin:\${PATH}\"" ${HOME}/.zshrc
+
+echo "${COND_INSERT} 'export PATH=\"${PWD}/bin:\${PATH}\"' \${HOME}/.bashrc"
+${COND_INSERT} "export PATH=\"${PWD}/bin:\${PATH}\"" ${HOME}/.bashrc
+
+
+cd ${CWD}
+
