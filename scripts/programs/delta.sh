3351a623e1f557e62fe366bd957c986ecbc21b5e
joehays <hays.joe@gmail.com>
2025-08-15 10:55:08 -0400

    fixing zdiff3 issue from 'git-deltas'

--- scripts/programs/delta.sh
+++ scripts/programs/delta.sh
@@ -25,6 +25,6 @@ sudo apt install git-delta
 git config --global core.pager delta
 git config --global interactive.diffFilter 'delta --color-only'
 git config --global delta.navigate true
-git config --global merge.conflictStyle zdiff3
+#git config --global merge.conflictStyle zdiff3
 
 cd ${CWD}
